8af72abb14fbd13bda271954d1557199256272c4
Ekaterina <katiai@yandex.ru>
2024-09-12 18:50:17 +0300

    hide accessKey

--- tech-internship_avito/NetworkManager/NetworkConstant.swift
+++ tech-internship_avito/NetworkManager/NetworkConstant.swift
@@ -9,11 +9,11 @@ import Foundation
 
 enum NetworkConstant {
     static let baseUrl = "https://api.unsplash.com"
-    static let accessKey = "fw7ga1yOJSqFG2yk_TAcv-4Dq6Z2MIlzzY0IRmYl-vA"
+    static let accessKey = "enter your key"
 }
 
 
-//static let accessKey = "enter your key"
+
 
 
 
